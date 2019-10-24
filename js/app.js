d9489671f12609e7a19f67d7a013ff144fb0727f
Zaynaib Giwa <zaynaibg@gmail.com>
2019-10-23 21:17:13 -0500

    comment on functionality of js file and add font awesome style sheet

--- js/app.js
+++ js/app.js
@@ -1,36 +1,42 @@
-//reference text: https://www.sitepoint.com/dom-manipulation-vanilla-javascript-no-jquery/
+
+//Variables
 
 let families=[{name:"Fascinate Inline",author:"Astigmatic"},{name:"Indie Flower",author:"Kimberly Flower"}
                 ,{name:"Roboto",author:"Christian Robertson"},{name:"Jomolhari",author:"Christopher J. Fynn"}
                 ,{name:"Liu Jian Mao Cao",author:"Liu Zhengjiang"},{name:"Modak",author:"Ek Type"}]
-//variables
 
 //grab users inputs
 
 let fontText = document.getElementById("fontText");
 let textSize = document.getElementById("fontSize");
 
-//methods
+//Methods
 
-//Takes text that the user inputs on to the html screen
-
+//Takes what the user types in and puts it onto the webpage
 let changingText = function(){
+
+    //grab all of the paragraph elements that have the class changeText
     let previews = document.querySelectorAll(".changeText");
-    previews.forEach(text =>  text.innerHTML = fontText.value)
+
+    //loop through each of the elements and uses an anonymous function to change the value to the user input value
+    previews.forEach(text =>  text.innerHTML = fontText.value);
 };
 
 //Changes size of the text 
 let changingSize = function(){
+    //grab all of the paragraph elements that have the class changeText
     let previews = document.querySelectorAll(".changeText");
+    //loop through each of the elements and uses an anonymous function to change the font size based on the user input.
     previews.forEach(text => text.style.fontSize = textSize.value + "px" );
 }
 
-//event listeners
+//Event Listeners
 
 fontText.addEventListener('keyup',changingText);
 textSize.addEventListener('keyup',changingSize);
 
-var makeCard = function(family) {
+//generates a  html template based on the metadata from google fonts
+let makeCard = function(family) {
     return `
 <link href="https://fonts.googleapis.com/css?family=${family.name}&display=swap" rel="stylesheet">
 <div class="card">
@@ -44,12 +50,18 @@ var makeCard = function(family) {
     <button class="add">+</button>
 
 </div>`
-}
+};
 
+//render function creates a google font card inside the div with the container class/
 function render() {
+    //select the container class
     let container = document.querySelector('.container');
-    let html = families.map(makeCard).join("")
+
+    //creates a new array and calls MakeCard function on each element
+    //joins each of the cards together
+    let html = families.map(makeCard).join("");
     container.innerHTML = html;
+    console.log(html)
 }
 
 render();
