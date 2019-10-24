de6bbc6fcc0bc47586b193e5c79545df0c9654dc
Zaynaib Giwa <zaynaibg@gmail.com>
2019-10-23 19:15:59 -0500

    complete basic requirements for chingu tier1 solo project

--- js/app.js
+++ js/app.js
@@ -1,32 +1,59 @@
 //reference text: https://www.sitepoint.com/dom-manipulation-vanilla-javascript-no-jquery/
 
+let families = ["Lato","Roboto","Oswald"];
+
 //variables
 
 //grab users inputs
 
 let fontText = document.getElementById("fontText");
 let textSize = document.getElementById("fontSize");
-let text = document.getElementById("changeText");
 
 //methods
 
 //Takes text that the user inputs on to the html screen
 
 let changingText = function(){
-    text.innerHTML = fontText.value;
+    let previews = document.querySelectorAll(".changeText");
+    previews.forEach(text =>  text.innerHTML = fontText.value)
 };
 
 //Changes size of the text 
 let changingSize = function(){
-    console.log(textSize.value);
-    text.style.fontSize = textSize.value + "px";
-
+    let previews = document.querySelectorAll(".changeText");
+    previews.forEach(text => text.style.fontSize = textSize.value + "px" );
 }
 
 //event listeners
 
-fontText.addEventListener('change',changingText);
-textSize.addEventListener('change',changingSize);
+fontText.addEventListener('keyup',changingText);
+textSize.addEventListener('keyup',changingSize);
 
+var makeCard = function(family) {
+    return `
+<link href="https://fonts.googleapis.com/css?family=${family}&display=swap" rel="stylesheet">
+<div class="card">
+    <div class ="metaData">
+        <p class="fontType">${family}</p>
+        <p class="author"></p>    
+    </div>
+    <p class="changeText" contentEditable style="font-family:${family}">Hi</p>
+    
 
+    <button class="add">+</button>
 
+</div>`
+}
+
+function render() {
+    let container = document.querySelector('.container');
+    let html = families.map(makeCard).join("")
+    container.innerHTML = html;
+}
+
+render();
+
+setTimeout(function() {
+    families.push("Lobster");
+    render();
+}, 3000);
